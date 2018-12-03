5feb591f1891b53f41ba98e6587fcf3f0a5d467f
Jordan Charest <jordancharest3@gmail.com>
2018-12-02 22:41:20 -0500

    Added signature negotiation

--- negotiation.py
+++ negotiation.py
@@ -2,8 +2,9 @@ import secrets
 
 
 all_protocols = {
-    "key_exchange" : ["DH", "RSA", "PSK", "ECC"],
-    "cipher" : ["BG", "AES/T-DES"],   # not sure right now about AES or Triple-DES
+    "key_exchange" : ["DH", "ECC"],
+    "cipher" : ["BG", "TDES"],
+    "signature" : "RSA",
     "HMAC" : "SHA-1"
 }
 
@@ -14,18 +15,21 @@ def handshake(server, role):
         for kep in client_protocols["key_exchange"]: print(kep, end=" ")
         print("\n  Ciphers: ", end="")
         for c in client_protocols["cipher"]: print(c, end=" ")
+        print("\n  Signature: ", client_protocols["signature"], end="")
         print("\n  HMAC: ", client_protocols["HMAC"])
 
         # pick at random from the options sent by the client
         print("\nMaking random choice. Using:")
         key_exchange = secrets.choice(client_protocols["key_exchange"])
         cipher = secrets.choice(client_protocols["cipher"])
-        hmac = "SHA-1"
-        print("  Key Exchange: {0}\n  Cipher: {1}\n  HMAC: {2}".format(key_exchange, cipher, hmac))
+        signature = client_protocols["signature"]
+        hmac = client_protocols["HMAC"]
+        print("  Key Exchange: {0}\n  Cipher: {1}\n  Signature: {2}\n  HMAC: {3}".format(key_exchange, cipher, signature, hmac))
 
         msg = {
             "key_exchange" : key_exchange,
             "cipher" : cipher,
+            "signature" : signature,
             "HMAC" : hmac
         }
         server.send(msg)
@@ -37,7 +41,8 @@ def handshake(server, role):
         key_exchange = protocol["key_exchange"]
         cipher = protocol["cipher"]
         hmac = protocol["HMAC"]
+        signature = protocol["signature"]
         print("Received server choice. Using:")
-        print("  Key Exchange: {0}\n  Cipher: {1}\n  HMAC: {2}".format(key_exchange, cipher, hmac))
+        print("  Key Exchange: {0}\n  Cipher: {1}\n  Signature: {2}\n  HMAC: {3}".format(key_exchange, cipher, signature, hmac))
 
-    return key_exchange, cipher, hmac
+    return key_exchange, cipher, signature, hmac
