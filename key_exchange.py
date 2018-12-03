2d7275efa919a94a898615ce3ebb0a63d8156adb
Jordan Charest <jordancharest3@gmail.com>
2018-12-02 22:40:46 -0500

    Removed unused key exchange algos

--- key_exchange.py
+++ key_exchange.py
@@ -1,30 +1,23 @@
 import secrets
 
-
+# -----------------------------------------------------------------------------
 def key_exchange(server, key_exchange, role):
 
     # FOR TESTING
-    key_exchange = "DH"
+    # key_exchange = "DH"
 
     if key_exchange == "DH":
         DH(server, role)
-    elif key_exchange == "RSA":
-        RSA(server, role)
     elif key_exchange == "ECC":
         ECC(server, role)
-    elif key_exchange == "PSK":
-        PSK(server, role)
-
 
 # -----------------------------------------------------------------------------
 def DH(server, role):
-    print("Running Diffie-Hellman Key Exchange")
-    # p = 19701948417508082028349673805013089501882820464569371835345485642790557819661
-    p_1024 = 137379528414317606724783492848081555720283075091870828195519131515167070861754597199829274387302328027015419063936033191563930078052582356021467556154341659725297054437914526417256171045697340225118653762315080757061962488124994375567389300922747400080660694665773568950985145405095127742611044300419476060483
-    # p_1024 = 23
+    print("\nRunning Diffie-Hellman Key Exchange")
+    p = 137379528414317606724783492848081555720283075091870828195519131515167070861754597199829274387302328027015419063936033191563930078052582356021467556154341659725297054437914526417256171045697340225118653762315080757061962488124994375567389300922747400080660694665773568950985145405095127742611044300419476060483
     g = 71
-    a = secrets.randbelow(p_1024-1)
-    public_key = pow(g, a, p_1024)
+    a = secrets.randbelow(p-1)
+    public_key = pow(g, a, p)
     if role == "server":
         print("Generated a private key and an exchange key. Sending exchange key to client")
         print("Generated key:", public_key)
@@ -41,20 +34,9 @@ def DH(server, role):
 
         server.send(public_key)
 
-
-
-    session_key = pow(received_key, a, p_1024)
+    session_key = pow(received_key, a, p)
     print("Determined session key:", session_key)
-
-
-# -----------------------------------------------------------------------------
-def RSA(server, role):
-    print("Running RSA Key Exchange")
 
 # -----------------------------------------------------------------------------
 def ECC(server, role):
     print("Running ECC Key Exchange")
-
-# -----------------------------------------------------------------------------
-def PSK(server, role):
-    print("Running PSK key-exchange")
