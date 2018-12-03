87c7153e1beec08d545bb146b33ed68bfe97907c
Jordan Charest <jordancharest3@gmail.com>
2018-12-03 10:07:21 -0500

    Compatibility fixes

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from TcpServer import TcpServer
 from negotiation import handshake
 from key_exchange import key_exchange
-from hashing import SHA1
+from hashing import HMAC
 from ciphers import Blum, TDES
 from signature import RSA
 
@@ -10,15 +10,21 @@ if __name__ == "__main__":
     port = 5000
     server = TcpServer(host, port, "client")
     key_exchange_algo, cipher_algo, signature_algo, hmac = handshake(server, "client")
-    key_exchange(server, key_exchange_algo, "client")
+    session_key = key_exchange(server, key_exchange_algo, "client")
+    bin_session_key = bin(session_key)[2:]
+    keys = []
+    for i in range(0, 256, 64):
+        keys.append(int(bin_session_key[i:i+64], 2))
 
     # get helper objects for encryption, hashing, and signature
-    if cipher_algo == "BG":
-        cipher = Blum()
-    elif cipher_algo == "TDES":
-        cipher = TDES()
+    # if cipher_algo == "BG":
+        # cipher = Blum()
 
-    hasher = SHA1()
+    cipher_algo = "TDES"
+    if cipher_algo == "TDES":
+        cipher = TDES(keys[0], keys[1], keys[2])
+
+    hmac_generator = HMAC(str(keys[3]))
 
     print("\nKeys have been exchanged. Ready to send messages.")
     print("Encrypting with {0}, hashing with {1} and signing with {2}".format(cipher_algo, signature_algo, hmac))
@@ -26,18 +32,20 @@ if __name__ == "__main__":
     # secure communication
     plaintext = input("--> ")
     while plaintext != "quit" or plaintext != "exit" or plaintext != "q":
-        hashed = hasher.hash(plaintext)
+        hashed = hmac_generator.compute(plaintext)
         print("Hashed the message:", hashed)
 
         # CHANGE THIS TO CIPHERTEXT ONCE THE CIPHERS ARE DONE
+        ciphertext = cipher.encrypt(plaintext)
         msg = {
-            "msg" : plaintext,
+            "msg" : ciphertext,
             "hash" : hashed
         }
         server.send(msg)
 
         # wait for response
         data = server.receive()
-        print('Received from server: ' + data)
+        print('Received from server: ',data)
+        response = cipher.decrypt(ciphertext)
 
         plaintext = input("--> ")
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from TcpServer import TcpServer
 from negotiation import handshake
 from key_exchange import key_exchange
-from hashing import SHA1
+from hashing import HMAC
 from ciphers import Blum, TDES
 from signature import RSA
 
@@ -11,15 +11,25 @@ if __name__ == "__main__":
     server = TcpServer(host, port)
 
     key_exchange_algo, cipher_algo, signature_algo, hmac = handshake(server, "server")
-    key_exchange(server, key_exchange_algo, "server")
+    session_key = key_exchange(server, key_exchange_algo, "server")
+    bin_session_key = bin(session_key)[2:]
+    keys = []
+    for i in range(0, 256, 64):
+        keys.append(int(bin_session_key[i:i+64], 2))
+    print(keys)
+
+
+
+
 
     # get helper objects for encryption, hashing, and signature
-    if cipher_algo == "BG":
-        cipher = Blum()
-    elif cipher_algo == "TDES":
-        cipher = TDES()
+    # if cipher_algo == "BG":
+    #     cipher = Blum()
+    cipher_algo = "TDES"
+    if cipher_algo == "TDES":
+        cipher = TDES(keys[0], keys[1], keys[2])
+    hmac_generator = HMAC(str(keys[3]))
 
-    hasher = SHA1()
     signature = RSA("rsa_secrets.txt")
 
     print("\nKeys have been exchanged. Ready to receive messages.")
@@ -33,7 +43,8 @@ if __name__ == "__main__":
 
         # decrypt and verify the hash
         plaintext = cipher.decrypt(ciphertext)
-        hash_verify = hasher.hash(plaintext)
+        print("Plaintext from client:", plaintext)
+        hash_verify = hmac_generator.compute(plaintext)
         if hash_verify != client_hash:
             print("Data integrity is compromised")
         else:
@@ -41,7 +52,7 @@ if __name__ == "__main__":
 
         # send the response, just the reverse of the plaintext
         ciphertext = cipher.encrypt(plaintext[::-1])
-        hashed = hasher.hash(plaintext[::-1])
+        hashed = hmac_generator.compute(plaintext[::-1])
         signed_hash = signature.sign(hashed)
         msg = {
             "msg" : ciphertext,
